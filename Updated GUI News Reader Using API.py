5c2170056564c94dff972cb14960e14c2c618cd3
Jwalant Modi <78471553+JwalantModi15@users.noreply.github.com>
2021-02-08 13:10:22 +0530

    Add files via upload

---
+++ Updated GUI News Reader Using API.py
@@ -0,0 +1,77 @@
+from tkinter import *
+import requests
+
+root = Tk()
+root.title("New Reader")
+root.geometry("1000x700")
+root.config(bg = "white")
+
+def main(s1):
+    news_text.config(state = "normal")
+    news_text.delete("1.0",END)
+    if s1 == "top headlines":
+        url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=0c1595a0d57a44d58264f33882471a7b"
+        res = requests.get(url)
+        data  = res.json()["articles"]
+        s = ""
+        n = 1
+        news_text.insert(END, "Top Headlines\n\n")
+        for i in data:
+            s = s + str(n) + ". " + i["title"] + "\n\n\n"
+            n+=1
+        news_text.insert(END, s)
+        news_text.config(state = "disable")
+
+    else:
+        url = "http://newsapi.org/v2/top-headlines?country=in&category="+s1+"&apiKey=0c1595a0d57a44d58264f33882471a7b"    
+        res = requests.get(url)
+        data  = res.json()["articles"]
+        s = ""
+        n = 1
+        news_text.insert(END, s1.capitalize()+" Headlines"+"\n\n")
+        for i in data:
+            s = s + str(n) + ". " + i["title"] + "\n\n\n"
+            n+=1
+        news_text.insert(END, s)
+        news_text.config(state = "disable")
+
+url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=0c1595a0d57a44d58264f33882471a7b"
+
+res = requests.get(url)
+
+data  = res.json()["articles"]
+s = ""
+
+scroll_v = Scrollbar(root)
+scroll_v.pack(side = RIGHT, fill = Y)
+
+scroll_h = Scrollbar(root, orient = 'horizontal')
+scroll_h.pack(side =BOTTOM, fill = X)
+
+news_text = Text(root, font = ("", 15), wrap = NONE, highlightthickness = 0, bd = 0, yscrollcommand = scroll_v.set, xscrollcommand = scroll_h.set)
+news_text.pack(pady = 10,padx = 10, expand = True, fill=BOTH)
+
+scroll_v.config(command = news_text.yview)
+scroll_h.config(command = news_text.xview)
+news_text.insert(END, "Top Headlines\n\n")
+n = 1
+for i in data:
+    s = s + str(n) + ". " + i["title"] + "\n\n\n"
+    n+=1
+news_text.insert(END, s)
+
+news_text.config(state = "disable")
+
+main_menu = Menu(root)
+choose_menu = Menu(main_menu, tearoff=0)
+choose_menu.add_cascade(label = "technology", command = lambda : main("technology"))
+choose_menu.add_cascade(label = "sports", command = lambda : main("sports"))
+choose_menu.add_cascade(label = "business", command = lambda : main("business"))
+choose_menu.add_cascade(label = "health", command = lambda : main("health"))
+choose_menu.add_cascade(label = "entertainment", command = lambda: main("entertainment"))
+choose_menu.add_cascade(label = "science", command = lambda : main("science"))
+choose_menu.add_cascade(label = "top headlines", command = lambda : main("top headlines"))
+main_menu.add_cascade(label = "News Topics", menu = choose_menu)
+root.config(menu = main_menu)
+
+root.mainloop()
