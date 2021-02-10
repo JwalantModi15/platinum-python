b1526d5d38240ea239d41f8c099f83de8da313d3
Jwalant Modi <78471553+JwalantModi15@users.noreply.github.com>
2021-02-10 17:41:46 +0530

    Add files via upload

---
+++ Arrange files in Appropriate Folder.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+def createfolder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+def move(folder_name,files):
+    for file in files:
+        shutil.move(file,f"{folder_name}/{file}")
+
+
+
+if __name__=='__main__':
+    s = r'C:\Users\Jwalant Modi\AppData\Local\Programs\Python\Python38-32\Python Programs\Practice Programs\Example'
+    os.chdir(s)    
+    files = os.listdir()
+    files.remove("Arrange files in Appropriate Folder.py")
+    
+    createfolder("Docs")
+    # createfolder("Imgs")
+    docExt = ['.txt','.docx','.pdf']
+    imgExt = ['.png','.ico','.jpg']
+    docs = []
+    # docs = [file for file in files if os.path.splitext(file)[1].lower() in docExt]
+    for file in files:
+        if os.path.splitext(file)[1].lower() in docExt:
+            docs.append(file)       
+
+    # imgs = [file for file in files if os.path.splitext(file)[1].lower() in imgExt]
+    move("Docs",docs)
+    # move("Imgs",imgs)
+
+
+    
